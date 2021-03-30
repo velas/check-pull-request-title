49386729f037f15fc3042c8caa3eb25f71ceb5a4
olexandr13 <oleksandr.pelykh@gmail.com>
2021-03-30 21:32:13 +0300

    initial

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,24 @@ import * as core from '@actions/core'
 import * as github from '@actions/github'
 
 function run() {
-  const pattern = core.getInput('pattern')
-  const regex = new RegExp(pattern)
+  // const pattern = core.getInput('pattern')
+  // const regex = new RegExp(pattern);
   const title =
     github.context.payload &&
     github.context.payload.pull_request &&
     github.context.payload.pull_request.title
   core.info(title)
-  const isValid = regex.test(title)
-  if (!isValid) {
-    core.setFailed(
-      `Pull request title "${title}" does not match regex pattern "${pattern}".`,
-    )
+  // const isValid = regex.test(title)
+  // if (!isValid) {
+  //   core.setFailed(
+  //     `Pull request title "${title}" does not match regex pattern "${pattern}".`,
+  //   )
+  // }
+
+  console.log('PULL REQUEST:');
+  console.log(JSON.stringify(github.context.payload.pull_request, null, 2));
+  if (github && github.context && github.context.payload && github.context.payload.pull_request && github.context.payload.pull_request.title) {
+    core.setOutput('title', github.context.payload.pull_request.title);
   }
 }
 
