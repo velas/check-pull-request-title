5530739c6d6b2dfc0d82370921f0c3a9bae84d4e
olexandr13 <oleksandr.pelykh@gmail.com>
2021-03-31 13:55:25 +0300

    add pr title validation

--- index.js
+++ index.js
@@ -2,29 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 function run() {
-  // const pattern = core.getInput('pattern')
-  // const regex = new RegExp(pattern);
-  // const title =
-  //   github.context.payload &&
-  //   github.context.payload.pull_request &&
-  //   github.context.payload.pull_request.title
-  // core.info(title)
-  // const isValid = regex.test(title)
-  // if (!isValid) {
-  //   core.setFailed(
-  //     `Pull request title "${title}" does not match regex pattern "${pattern}".`,
-  //   )
-  // }
+  const title = github.context.payload.pull_request.title;
+  const prURL = github.context.payload.pull_request._links.html;
+
+  if (!title) throw new Error(`Not title provided`);
+  // const title = 'VTX-100, vtx-111 some text () bla bla []';
+  const pattern = 'vtx-\\d+';
+  const regex = new RegExp(pattern, 'gim');
+  core.info(`Title: ${title}`);
+  const matches = title.match(regex);
+  core.info(`Matches: ${matches}`);
+  const isValid = !!matches.length;
+
+  core.info(`Is valid: ${isValid}`);
+  if (!isValid) {
+    core.setFailed(
+      `Pull request title "${title}" does not match regex pattern "${pattern}".`,
+    )
+  }
 
   // console.log('PULL REQUEST:');
   // console.log(JSON.stringify(github.context.payload.pull_request, null, 2));
-  console.log('TITLE:');
-  console.log(github.context.payload.pull_request.title);
-  console.log('URL:');
-  console.log(github.context.payload.pull_request._links.html);
+  
+  console.log(`Title: ${title}`);
+  console.log(`URL: ${prURL}`);
   if (github && github.context && github.context.payload && github.context.payload.pull_request) {
-    core.setOutput('title', github.context.payload.pull_request.title);
-    core.setOutput('url', github.context.payload.pull_request._links.html.href);
+    core.setOutput('title', title);
+    core.setOutput('url', prURL);
   }
 }
 
